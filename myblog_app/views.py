d5ecf03ac1006e9353a8d48c39119e278bf13292
insolor <insolor@gmail.com>
2022-03-30 17:04:38 +0300

    Карточки постов на главной странице

--- myblog_app/views.py
+++ myblog_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 
+from myblog_app.models import Post
+
+
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            'myblog_app/home.html'
-        )
+        posts = Post.objects.all()
+        return render(request, 'myblog_app/home.html', context={
+            'posts': posts
+        })
